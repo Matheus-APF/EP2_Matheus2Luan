9230a30c99ca73cbd8d25fffbbd8821b84dcacce
Matheus <matheusapf@al.insper.edu.br>
2025-04-29 20:05:50 -0300

    remover_dado

--- funcoes.py
+++ funcoes.py
@@ -6,8 +6,14 @@ def rolar_dados(vezes):
         dados.append(dado)
     return dados
 
-def guardar_dado(rolados,estoque, i_guarda):
-    dado = rolados[i_guarda] #encontra o dado
-    del rolados[i_guarda]    #remove dos rolados
-    estoque.append(dado)     #adiciona ao estoque
+def guardar_dado(rolados, estoque, i_guarda):
+    dado = rolados[i_guarda] # Encontra o dado
+    del rolados[i_guarda]    # Remove dos rolados
+    estoque.append(dado)     # Adiciona ao estoque
+    return [rolados, estoque]
+
+def remover_dado(rolados, estoque, i_guarda):
+    dado = estoque[i_guarda]  # Encontra o dado no estoque
+    del estoque[i_guarda]     # Remove do estoque
+    rolados.append(dado)      # Adiciona novamente aos rolados
     return [rolados, estoque]
