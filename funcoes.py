33759a4921fce09f4b15126268ff359ed3952235
Matheus <matheusapf@al.insper.edu.br>
2025-04-30 14:49:52 -0300

    calcula_pontos_full_house

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 import random
+# 1
 def rolar_dados(vezes):
     dados = []                       #Estoque dos dados
     for i in range(vezes):           #Rola o dado o numero de vezes requisitada
@@ -6,28 +7,35 @@ def rolar_dados(vezes):
         dados.append(dado)           #Armazena o valor no Estoque
     return dados
 
+# 2
 def guardar_dado(rolados, estoque, i_guarda):
     dado = rolados[i_guarda] # Encontra o dado
     del rolados[i_guarda]    # Remove dos rolados
     estoque.append(dado)     # Adiciona ao estoque
     return [rolados, estoque]
 
+# 3
 def remover_dado(rolados, estoque, i_guarda):
     dado = estoque[i_guarda]  # Encontra o dado no estoque
     del estoque[i_guarda]     # Remove do estoque
     rolados.append(dado)      # Adiciona novamente aos rolados
     return [rolados, estoque]
 
+# 4
 def calcula_pontos_regra_simples(dados_rolados):
     pontos = {}
     for categoria in range(1, 7):   # Verifica dados em cada categoria
         pontos[categoria] = dados_rolados.count(categoria) * categoria # Pontos: categoria * n° dados se enquadram nela(.count)
     return pontos
+
+# 5
 def calcula_pontos_soma(dados):
     soma = 0
     for i in dados:
         soma += i
     return soma
+
+# 6
 def calcula_pontos_sequencia_baixa(dados):
     um = dois = tres = quatro = cinco = seis = False
     for i in dados:
@@ -51,6 +59,8 @@ def calcula_pontos_sequencia_baixa(dados):
         return 15
     else:
         return 0
+
+# 7
 def calcula_pontos_sequencia_alta(dados):
     um = dois = tres = quatro = cinco = seis = False
     for i in dados:
@@ -70,5 +80,26 @@ def calcula_pontos_sequencia_alta(dados):
         return 30
     elif dois == True and tres == True and quatro == True and cinco == True and seis ==True:
         return 30
+    else:
+        return 0
+
+# 8   
+def calcula_pontos_full_house(dados):
+    # Cria variaveis p modificar
+    house = {}
+    soma = 0
+
+    # Cria dicionário com ocorrência de cada face
+    for d in dados:
+        soma += d
+        if d not in house.keys():
+            house[d] = 1
+        else:
+            house[d] += 1
+
+    # Verifica o caso full_house : duas faces distintas(len() == 2) repetidas 3 e 2 vezes(3 in and 2 in)
+    valores = house.values()
+    if len(valores) == 2 and (3 in valores and 2 in valores):
+        return soma
     else:
         return 0
